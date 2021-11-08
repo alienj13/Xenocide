9dbef8f5cfc3dbfdbf05459a9a85294509af4222
IBN5101 <minhnghcm123@gmail.com>
2021-11-08 14:52:58 +0000

    Added diamond pattern to "PatternGen"
    Diamond pattern yay.

--- Assets/Scripts/Util/PatternGen.cs
+++ Assets/Scripts/Util/PatternGen.cs
@@ -6,7 +6,7 @@ public class PatternGen : MonoBehaviour
 {
 
     // Temporary solution
-    // Using Vector2I
+    // Using Vector2Int and x,y
     public static List<Vector2Int> square(int x, int y, int range) {
         List<Vector2Int> results = new List<Vector2Int>();
 
@@ -27,5 +27,27 @@ public class PatternGen : MonoBehaviour
         return results;
     }
 
+    // Temporary solution
+    // Using Vector2Int and x,y
+    public static List<Vector2Int> diamond(int x, int y, int range) {
+        List<Vector2Int> results = new List<Vector2Int>();
+
+        // Temporary solution: Hard-coded limits
+        int width = 10;
+        int height = 10;
+
+        for (int i = (x - range); i <= (x + range); i++) {
+            int k = Mathf.Abs(Mathf.Abs(i - x) - range);
+            for (int j = (y - k); j <= (y + k); j++) {
+                if (i == x && j == y)
+                    continue;
+                if (i < 0 || j < 0 || i >= width || j >= height)
+                    continue;
+                results.Add(new Vector2Int(i, j));
+            }
+        }
+
+        return results;
+    }
 
 }
