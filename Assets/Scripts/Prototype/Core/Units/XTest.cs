2fa4b1699732b383fa6681090aaf1de13887ff4d
IBN5101 <minhnghcm123@gmail.com>
2021-12-06 22:42:20 +0000

    Changed XTest unit to have new move/attack
    Because I am bored. XTest now attack the first unit on the line in front of it. Also increased ATK to 200, because why not.

--- Assets/Scripts/Prototype/Core/Units/XTest.cs
+++ Assets/Scripts/Prototype/Core/Units/XTest.cs
@@ -4,24 +4,56 @@ using UnityEngine;
 
 public class XTest : Unit
 {
-    private int moveRange = 3;
-    // TODO: Improve this
-    private int attackRangeMin = 4;
-    private int attackRangeMax = 6;
+    private Vector2Int[] directions =
+    {
+        Vector2Int.left,
+        Vector2Int.right,
+    };
+    private int moveRange = 20;
 
     public override HashSet<Vector2Int> GenerateAvailableMoves()
     {
         ClearMoves();
-        AddMoves(PatternGen.DiamondMove(this, moveRange));
+        AddMoves(PatternGen.LineMove(this, directions, moveRange));
         return availableMoves;
     }
 
     public override HashSet<Vector2Int> GenerateAvailableAttacks()
     {
         ClearAttacks();
-        // TODO: Improve this
-        AddAttacks(PatternGen.DiamondAttack(this, attackRangeMax));
-        RemoveAttacks(PatternGen.DiamondAttack(this, attackRangeMin - 1));
+        PlayerTeam oppositeTeam = (this.Team == PlayerTeam.P1) ? PlayerTeam.P2 : PlayerTeam.P1;
+        Vector2Int direction = (this.Team == PlayerTeam.P1) ? Vector2Int.up : Vector2Int.down;
+        // For opposite team unit
+        Unit unit = GetUnitInDirection<Unit>(oppositeTeam, direction);
+        if (unit)
+            AddAttack(unit.OccupiedSquare);
+        // For neutral units
+        unit = GetUnitInDirection<Unit>(PlayerTeam.Neutral, direction);
+        if (unit)
+            AddAttack(unit.OccupiedSquare);
         return availableAttacks;
     }
+
+    // Note: Copy this to a ranged unit in the future.
+
+    //private int moveRange = 3;
+    //// TODO: Improve this
+    //private int attackRangeMin = 4;
+    //private int attackRangeMax = 6;
+
+    //public override HashSet<Vector2Int> GenerateAvailableMoves()
+    //{
+    //    ClearMoves();
+    //    AddMoves(PatternGen.DiamondMove(this, moveRange));
+    //    return availableMoves;
+    //}
+
+    //public override HashSet<Vector2Int> GenerateAvailableAttacks()
+    //{
+    //    ClearAttacks();
+    //    // TODO: Improve this
+    //    AddAttacks(PatternGen.DiamondAttack(this, attackRangeMax));
+    //    RemoveAttacks(PatternGen.DiamondAttack(this, attackRangeMin - 1));
+    //    return availableAttacks;
+    //}
 }
