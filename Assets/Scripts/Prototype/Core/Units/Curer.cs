6a1c552f8fdc057554793a991a309619bb1a9236
IBN5101 <73728839+IBN5101@users.noreply.github.com>
2022-03-10 19:09:21 +0000

    Added scripts for future units
    Curer and Destroyer.

---
+++ Assets/Scripts/Prototype/Core/Units/Curer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Curer : Unit
+{
+    private int moveRange = 5;
+    private int attackRange = 1;
+
+    public override HashSet<Vector2Int> GenerateAvailableMoves()
+    {
+        AddMoves(PatternGen.SquareMove(this, moveRange));
+        return availableMoves;
+    }
+
+    public override HashSet<Vector2Int> GenerateAvailableAttacks()
+    {
+        AddAttacks(PatternGen.SquareAttack(this, attackRange));
+        return availableAttacks;
+    }
+}
---
+++ Assets/Scripts/Prototype/Core/Units/Destroyer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : Unit
+{
+    private int moveRange = 5;
+    private int attackRange = 1;
+
+    public override HashSet<Vector2Int> GenerateAvailableMoves()
+    {
+        AddMoves(PatternGen.SquareMove(this, moveRange));
+        return availableMoves;
+    }
+
+    public override HashSet<Vector2Int> GenerateAvailableAttacks()
+    {
+        AddAttacks(PatternGen.SquareAttack(this, attackRange));
+        return availableAttacks;
+    }
+}
