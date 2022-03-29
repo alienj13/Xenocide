87f3158923bc3c3b32a11ab396974780fbca0266
IBN5101 <73728839+IBN5101@users.noreply.github.com>
2022-03-29 22:46:45 +0100

    Added gridlines
    Still deciding on colors.

--- Assets/Scripts/Prototype/Core/GameControllers/GameController.cs
+++ Assets/Scripts/Prototype/Core/GameControllers/GameController.cs
@@ -50,6 +50,9 @@ public abstract class GameController : MonoBehaviour
 
         UIManager.OnGameStarted();
 
+        // Create gridlines
+        CreateGridlines();
+
         // Moved creating units to TryToStartCurrentGame()
         //CreateUnitsFromLayout(startingFieldLayout);
         activePlayer = player1;
@@ -112,6 +115,35 @@ public abstract class GameController : MonoBehaviour
 
         XPlayer currentPlayer = (team == PlayerTeam.P1) ? player1 : player2;
         currentPlayer.AddUnit(newUnit);
+    }
+
+    public void CreateGridlines()
+    {
+        Vector3 fieldPos = field.gameObject.transform.position;
+        //Vector3 fieldPosX = new Vector3(fieldPos.x - field.transform.localScale.x * 0.5f, fieldPos.y, fieldPos.z);
+        //Vector3 fieldPosZ = new Vector3(fieldPos.x, fieldPos.y, fieldPos.z - field.transform.localScale.z * 0.5f);
+        Vector3 blc = field.bottomLeftCornerTransform.position;
+        float squareSize = field.squareSize;
+        Vector3 fieldPosX = new Vector3(
+            blc.x, -0.5f, (blc.z + field.transform.localScale.z * 0.5f - squareSize));
+        Vector3 fieldPosZ = new Vector3(
+            (blc.x + field.transform.localScale.x * 0.5f - squareSize), -0.5f, blc.z);
+        
+        // Vertical gridlines
+        for (int i = 0; i <= 21; i++)
+        {
+            GameObject gridline = unitCreator.CreateGridline(0);
+            gridline.transform.position = new Vector3(
+                fieldPosX.x + squareSize * i, fieldPosX.y, fieldPosX.z);
+        }
+        // Horizontal gridlines
+        for (int i = 0; i <= 21; i++)
+        {
+            GameObject gridline = unitCreator.CreateGridline(1);
+            gridline.transform.position = new Vector3(
+                fieldPosZ.x, fieldPosZ.y, fieldPosZ.z + squareSize * i);
+        }
+
     }
 
     public bool IsTeamTurnActive(PlayerTeam team)
